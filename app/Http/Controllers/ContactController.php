15ef8103fa166121bc803ce3a1b480a4eabe75f7
Hanzo8885 <ohong02@gmail.com>
2026-06-15 10:05:58 +0700

    fix: memaksa form menggunakan https

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -7,24 +7,24 @@ use Illuminate\Support\Facades\Mail;
 
 class ContactController extends Controller
 {
-    public function send(Request $request)
-    {
-        // Menyusun data dari form kontak
-        $data = [
-            'first_name' => $request->first_name,
-            'last_name'  => $request->last_name,
-            'email'      => $request->email,
-            'subject'    => $request->subject,
-            'pesan'      => $request->message,
-        ];
+   public function send(Request $request)
+{
+    // Menyusun data dari form kontak
+    $data = [
+        'first_name' => $request->first_name,
+        'last_name'  => $request->last_name,
+        'email'      => $request->email,
+        'subject'    => $request->subject,
+        'pesan'      => $request->message,
+    ];
 
-        // Mengirim email secara inline menggunakan view template tanpa memanggil class ContactMail
-        Mail::send('emails.contact', $data, function ($message) use ($data) {
-            $message->to('ohong02@gmail.com')
-                    ->replyTo($data['email'], $data['first_name'] . ' ' . $data['last_name'])
-                    ->subject('📩 Pesan Baru: ' . ($data['subject'] ?? 'Form Kontak'));
-        });
+    // Menggunakan Mail::send standar Laravel agar bebas eror sintaksis VS Code
+    \Illuminate\Support\Facades\Mail::send('emails.contact', $data, function ($message) use ($data) {
+        $message->to('ohong02@gmail.com')
+                ->replyTo($data['email'], $data['first_name'] . ' ' . $data['last_name'])
+                ->subject('📩 Pesan Baru: ' . ($data['subject'] ?? 'Form Kontak'));
+    });
 
-        return back()->with('success', 'Pesan Anda berhasil dikirim!');
-    }
+    return back()->with('success', 'Pesan Anda berhasil dikirim!');
+}
 }
