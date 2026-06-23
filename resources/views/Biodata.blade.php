ffef7609d798d60fa1e49fd778a248396299935b
Hanzo8885 <ohong02@gmail.com>
2026-06-23 11:35:07 +0700

    Menambahkan gambar hobby

--- resources/views/Biodata.blade.php
+++ resources/views/Biodata.blade.php
@@ -128,7 +128,7 @@
 
         <div class="biodata-info">
             <div class="biodata-name">Alfikar Radestian Prasenja</div>
-            <div class="biodata-role">Student &amp; Creative Designer</div>
+            <div class="biodata-role">Siwa &amp; Jurusan Rpl</div>
 
             <table class="biodata-table">
                 <tr>
